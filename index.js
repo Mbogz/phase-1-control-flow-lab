65480e2f0047879ecbf9732c0d3ce7b296b16318
Mbogz <jeff.mwaura@student.moringaschool.com>
2023-04-05 21:08:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(cat){
+  if (cat <= 400){
+    return "It is a free ride!";
+  }
+  else if (cat > 2000){
+    return "It will only cost you thirty bucks.";
+  }
+  else if (cat > 2500){
+    return "No go";
+  }
 
 function ternaryCheckCity(){
-  // Write your code here!
-}
+ if (city === "NYC"){
+  return "Ok, sounds good.";
+ }
+ else{
+  return "No go.";
+ }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === "generous"){
+    return "Thank you so much.";
+  }
+  else if (tip === "not as generous"){
+    return "Thank you.";
+  
+  }
+  else{
+    return "Bye.";
+  }
 }
